2e90320b1d92d3896931f9d855c734e55b5db605
Neus Roman <neusroman@hotmail.com>
2024-10-13 09:25:19 +0200

    customize upload widget

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,30 @@
   <div>
     <h1>holaaa</h1>
     <div>
-      <CldUploadWidget v-slot="{ open }" uploadPreset="nuxt-cloudinary-unsigned">
+      <CldUploadWidget v-slot="{ open }"
+                       uploadPreset="nuxt-cloudinary-unsigned"
+                       :options="{
+                        sources: ['local', 'camera'],
+                        multiple: false,
+                        maxFiles: 1,
+                        styles: {
+                          palette: {
+                            window: 'black',
+                            windowBorder: 'orange',
+                            tabIcon: '#FFC156',
+                            menuIcons: 'orange',
+                            textDark: '#000000',
+                            textLight: 'orange',
+                            link: 'orange',
+                            action: 'orange',
+                            inactiveTabIcon: 'orange',
+                            error: 'red',
+                            inProgress: 'orange',
+                            complete: 'orange',
+                            sourceBg: '#151515'
+                          },
+                        }
+                       }">
         <button type="button" @click="open">Upload an Image</button>
       </CldUploadWidget>
     </div>
